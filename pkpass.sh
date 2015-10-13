8afd14513e2bec38372fe382b60c4df6285a6467
jackchammons <jackchammons@gmail.com>
2015-10-13 01:48:51 -0400

    capitalization of wwdr.pem

--- pkpass.sh
+++ pkpass.sh
@@ -58,7 +58,7 @@ then
 
    echo "SIGNING ITSELF"
 
-   openssl smime -binary -sign -certfile WWDR.pem -signer certificate.pem -inkey key.pem -passin pass:simplepassword -in "$2/manifest.json" -out "$2/signature" -outform DER
+   openssl smime -binary -sign -certfile wwdr.pem -signer certificate.pem -inkey key.pem -passin pass:simplepassword -in "$2/manifest.json" -out "$2/signature" -outform DER
    
    echo "Cleaning up"
 
